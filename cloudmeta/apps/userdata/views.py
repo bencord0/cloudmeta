f1f0b173ee1808de45c485fb163fb263841629ca
Ben Cordero (bencord0) <bmc@linux.com>
2013-08-06 15:27:09 +0100

    Oops, Node is not the node we're looking for

--- cloudmeta/apps/userdata/views.py
+++ cloudmeta/apps/userdata/views.py
@@ -2,7 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
 from cloudmeta.apps.userdata.models import Node
-from cloudmeta.apps.metadata.views import _get_node
+
+def _get_node(request):
+    remote_addr = request.META['HTTP_X_REAL_IP'] or request.META['REMOTE_ADDR']
+    try:
+        node = Node.objects.get(name=remote_addr)
+    except Node.DoesNotExist:
+        # Possible ipv4->ipv6 conversion?
+        node = get_object_or_404(Node, name=remote_addr.lstrip('::ffff:'))
+    return node
 
 def index(request):
     return HttpResponse("user-data service: {meta[REMOTE_ADDR]}".format(meta=request.META))

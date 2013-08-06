796a3c3376add31659365856c0490558b865e13a
Ben Cordero (bencord0) <bmc@linux.com>
2013-08-06 16:09:34 +0100

    strip crlf in userdata scripts

---
+++ cloudmeta/apps/userdata/migrations/0002_strip_crlf.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        for ud in orm.UserData.objects.all():
+            # ud is the old UserData model, it does not have the new
+            # .save() method to do the stripping for us.
+            ud.data = '\n'.join(ud.data.split('\n'))
+            ud.save()
+
+    def backwards(self, orm):
+        # It is save to store stripped userdata
+        pass
+
+    models = {
+        u'userdata.node': {
+            'Meta': {'object_name': 'Node'},
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '256', 'primary_key': 'True'}),
+            'userdata': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['userdata.UserData']"})
+        },
+        u'userdata.userdata': {
+            'Meta': {'object_name': 'UserData'},
+            'data': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '256'})
+        }
+    }
+
+    complete_apps = ['userdata']
+    symmetrical = True
--- cloudmeta/apps/userdata/models.py
+++ cloudmeta/apps/userdata/models.py
@@ -11,5 +11,11 @@ class UserData(models.Model):
     name = models.CharField(unique=True, max_length=256)
     data = models.TextField()
 
+    def save(self, *args, **kwargs):
+        def strip_crlf(self, text):
+            return '\n'.join(text.split('\r\n'))
+        self.data = strip_crlf(self.data)
+        super(UserData, self).save(*args, **kwargs)
+
     def __unicode__(self):
         return self.name
